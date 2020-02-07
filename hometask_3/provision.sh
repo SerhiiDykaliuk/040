d215cfb41aa6b13dacd8991801b8723e2ce1970e
SerhiiDykaliuk <breeck.ua@gmail.com>
2020-02-07 23:50:18 +0200

    Vagrantfile + script (Confluence only)

---
+++ hometask_3/provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# hometask 3 provision script
+# Installing Atlassian Confluence server
+# Instdlling MySQL DB  (ver.5.7)
+#	user: root  password:'123456',
+#	DBname: "CONFLUENCE"
+
+# making .bin file executable
+chmod u+x /vagrant/atlassian-confluence-7.3.1-x64.bin
+
+#running .bin
+/vagrant/atlassian-confluence-7.3.1-x64.bin
+
+
