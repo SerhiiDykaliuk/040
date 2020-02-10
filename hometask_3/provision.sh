99aeb489f847f67c48970264dfa5d2e6d887ddbb
SerhiiDykaliuk <breeck.ua@gmail.com>
2020-02-10 18:03:28 +0200

    first running on Centos

--- hometask_3/provision.sh
+++ hometask_3/provision.sh
@@ -6,10 +6,19 @@
 #	user: root  password:'123456',
 #	DBname: "CONFLUENCE"
 
+# jre installing
+#apt-get update
+#apt-get -y install default-jre
+
 # making .bin file executable
-chmod u+x /vagrant/atlassian-confluence-7.3.1-x64.bin
+ chmod u+x /vagrant/atlassian-confluence-7.3.1-x64.bin
 
 #running .bin
-/vagrant/atlassian-confluence-7.3.1-x64.bin
+/vagrant/atlassian-confluence-7.3.1-x64.bin -q -varfile response.varfile
 
+# installing mysql
+yum -y localinstall https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
+yum -y install mysql-community-server
+systemctl start mysqld
+systemctl enable mysqld
 
